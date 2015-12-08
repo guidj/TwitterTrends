2a2133a5a107aeac60476f6972220f455106f985
Guilherme Dinis Jr <guilherme.dinisjunior@gmail.com>
2015-12-08 16:56:55 +0100

    added config reader. closes #3

---
+++ src/main/java/com/gpjpe/twittertrends/CONFIG.java
@@ -0,0 +1,19 @@
+package com.gpjpe.twittertrends;
+
+public enum CONFIG {
+    API_KEY("ApiKey"),
+    API_SECRET("ApiSecret"),
+    TOKEN("Token"),
+    TOKEN_SECRET("TokenSecret"),
+    STREAM_ENDPOINT("StreamEndpoint");
+
+    private String name;
+
+    private CONFIG(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
---
+++ src/main/java/com/gpjpe/twittertrends/SecretsConfig.java
@@ -0,0 +1,39 @@
+package com.gpjpe.twittertrends;
+
+import org.apache.log4j.Logger;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class SecretsConfig {
+
+    private static String SECRETS_FILE = "secrets.properties";
+    private final static Logger LOGGER = Logger.getLogger(App.class.getName());
+
+    Properties properties;
+
+    public SecretsConfig() {
+        InputStream inputStream;
+
+        properties = new Properties();
+
+        inputStream = getClass().getClassLoader().getResourceAsStream(SECRETS_FILE);
+
+        try {
+            if (inputStream != null) {
+                properties.load(inputStream);
+            } else {
+                throw new FileNotFoundException("property file '" + SECRETS_FILE + "' not found in the classpath");
+            }
+        } catch (IOException e) {
+            LOGGER.error(e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getProperty(CONFIG key){
+        return this.properties.getProperty(key.getName());
+    }
+}
