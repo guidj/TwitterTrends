2288a1dced554203518b0cdd90ed399b170b6104
Guilherme Dinis Jr <guilherme.dinisjunior@gmail.com>
2015-12-17 20:52:19 +0100

    master2015.Top3App for appassembler, #11

---
+++ src/main/java/master2015/Top3App.java
@@ -0,0 +1,13 @@
+package master2015;
+
+import com.gpjpe.twittertrends.App;
+import org.apache.log4j.Logger;
+
+public class Top3App {
+
+    private final static Logger LOGGER = Logger.getLogger(Top3App.class.getName());
+
+    public static void main(String args[]) throws InterruptedException {
+        App.main(args);
+    }
+}
